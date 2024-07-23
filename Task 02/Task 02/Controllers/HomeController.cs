36b68bbe9be1cee8df36ad21d1ec66d66579143d
MoFawarah <mfawarah1@gmail.com>
2024-07-23 23:44:40 +0300

    updated

---
+++ Task 02/Task 02/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task_02.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SubmitContact(string Name, string Phone, string Gender, string Country, string[] Hobbies)
+        {
+            ViewBag.Name = Name;
+            ViewBag.Phone = Phone;
+            ViewBag.Gender = Gender;
+            ViewBag.Country = Country;
+            ViewBag.Hobbies = Hobbies;
+
+            return View("ContactResult");
+        }
+
+        public ActionResult ContactResult() { 
+        return View();
+        }
+    }
+}
