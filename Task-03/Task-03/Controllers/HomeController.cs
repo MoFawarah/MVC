b36da4d2d002e4b3605d5895c96a2686a10d2847
MoFawarah <mfawarah1@gmail.com>
2024-07-24 19:24:02 +0300

    updated

---
+++ Task-03/Task-03/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task_03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Session["Loggedin"] = "0";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact(FormCollection form, string[] food)
+        {
+            //ViewBag.Message = "Your contact page.";
+            string email = form["email"];
+            string city = form["selectcity"];
+            food = form.GetValues("selectfood");
+            string feedback = form["feedback"];
+
+            ViewBag.Email = email;
+            ViewBag.City = city;
+            ViewBag.Feedback = feedback;
+            ViewBag.Food = food;
+
+
+            return View();
+        }
+
+        public ActionResult Login(FormCollection form)
+        {
+
+            string[] userData = { "mfawarah1@gmail.com", "2016975037" };
+            string email = form["email"];
+            string password = form["password"];
+
+            if (email == userData[0] && password == userData[1])
+            {
+                Session["Loggedin"] = "1";
+                return View("Index");
+                
+            }
+            else
+            {
+                Session["Loggedin"] = "0";
+                return View();
+            }
+
+
+        }
+
+  
+    }
+}
